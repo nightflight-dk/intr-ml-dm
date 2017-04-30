b285fbe7bd46dec509100535c92015a4d92d6191
Damian <dakowalc@hotmail.com>
2017-04-30 22:44:27 +0200

    hierarchical-clust-hp

---
+++ hierarch-clustering-hp.R
@@ -0,0 +1,65 @@
+source("setup.R")
+
+library(R.matlab)
+library(nnet)
+library(cvTools)
+library(mclust) # install.packages("mclust") # package that can be used to fit a gaussian mixture model. This package does not allow random starts of the algorithm. It is faster than the algorithm in mixtools.
+
+graphics.off()
+
+cars <- data.frame(dat)
+
+X <- cars
+y <- cars$powerclass
+X <- cars[,!(names(cars) == "powerclass")]
+N <- nrow(cars)
+M <- length(X)
+
+attributeNames <- colnames(X)
+
+Xdf <- data.frame(X)
+colnames(Xdf) <- attributeNames
+
+# ## Gaussian mixture model
+# 
+# # Number of clusters
+# K = 4; #minimum value is 2
+# 
+# # Fit model
+# model <- mclust::Mclust(data=Xdf, G=K) # using the mclust package
+# # model <- mvnormalmixEM(x=Xdf, k=K, maxit=100, epsilon=1e-2, verb=TRUE) # using the mixtools package. Defaults for maxit and epsilon are 500 and 1e-8, respectively. Avoid extreme running times by allowing fewer iterations and deeming convergence earlier by setting maxit and epsilon as done here. The argument verb=TRUE makes the method write output from the EM algorithm at each iteration. The argument verb is FALSE by default.
+# 
+# # Get clustering
+# i = model$classification # using the mclust package
+# #i = max_idx(model$posterior) # using the mixtools package
+# 
+# # Get cluster centers
+# Xc = t(model$parameters$mean) # using the mclust package
+# #Xc = matrix(unlist(model$mu), nrow=length(model$mu), byrow=TRUE) # using the mixtools package
+# 
+# ## Plot results
+# # Plot clustering
+# clusterplot(Xdf, y, i, Xc, main='GMM: Clustering');
+
+############### hierarchical
+
+## Hierarchical clustering
+# Maximum number of clusters
+Maxclust = 4;
+
+# Compute hierarchical clustering
+# Methods <- c("ward","ward.D2","single","complete","average")
+hc <- hclust(dist(Xdf), method="ward.D2")
+
+# Compute clustering by thresholding the dendrogram
+i <- cutree(hc, k = Maxclust)
+
+## Plot results
+
+# Plot dendrogram
+plot(hc)
+
+# Plot data
+#dev.new()
+clusterplot(Xdf, y, i, main='Hierarchical')
+
